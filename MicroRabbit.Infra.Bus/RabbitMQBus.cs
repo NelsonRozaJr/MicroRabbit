0c03e1b96dd271ad2c9d3b0af86baa0ff67f8975
Nelson Roza Junior <jr.1403@hotmail.com>
2020-09-25 19:04:03 -0300

    Fix condition

--- MicroRabbit.Infra.Bus/RabbitMQBus.cs
+++ MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -129,16 +129,14 @@ namespace MicroRabbit.Infra.Bus
                 foreach (var subscription in subscriptions)
                 {
                     var handler = Activator.CreateInstance(subscription);
-                    if (handler == null)
+                    if (handler != null)
                     {
-                        continue;
+                        var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
+                        var @event = JsonConvert.DeserializeObject(message, eventType);
+                        var concreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
+
+                        await (Task)concreteType.GetMethod("Handle").Invoke(handler, new object[] { @event });
                     }
-
-                    var eventType = _eventTypes.SingleOrDefault(t => t.Name == eventName);
-                    var @event = JsonConvert.DeserializeObject(message, eventType);
-                    var concreteType = typeof(IEventHandler<>).MakeGenericType(eventType);
-
-                    await (Task)concreteType.GetMethod("Handle").Invoke(handler, new object[] { @event });
                 }
             }
         }
