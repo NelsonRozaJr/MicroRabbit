ffd24f4ce1070161f6695d0f7169c0f103b6f242
Nelson Roza Junior <jr.1403@hotmail.com>
2020-11-01 18:52:46 -0300

    Covariance and contravariance

--- MicroRabbit.Domain.Core/Bus/IEventHandler.cs
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -3,6 +3,34 @@ using System.Threading.Tasks;
 
 namespace MicroRabbit.Domain.Core.Bus
 {
+    /*
+        // Covariance and contravariance
+
+        // This interface can be implicitly cast to LESS DERIVED (upcasting)
+        public interface ICovariant<out T> { }
+
+        // This interface can be implicitly cast to MORE DERIVED (downcasting)
+        public interface IContravariant<in T> { }
+
+        public class Covariant<T> : ICovariant<T> { }
+        public class Contravariant<T> : IContravariant<T> { }
+
+        public class Fruit { }
+        public class Apple : Fruit { }
+
+        public class Program
+        {
+	        public static void Main()
+	        {
+		        // apple is being upcasted to fruit, without the out keyword this will not compile
+		        ICovariant<Fruit> fruit = new Covariant<Apple>();
+
+		        // fruit is being downcasted to apple, without the in keyword this will not compile
+		        IContravariant<Apple> apple = new Contravariant<Fruit>();
+	        }
+        }
+    */
+
     public interface IEventHandler<in TEvent> : IEventHandler where TEvent : Event
     {
         Task Handle(TEvent @event);
