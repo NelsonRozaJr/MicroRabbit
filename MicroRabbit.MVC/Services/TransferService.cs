3191c036b10e291c9ec232da88dbbbf9895e0b03
Nelson Roza Junior <jr.1403@hotmail.com>
2020-11-07 22:10:17 -0300

    MicroRabbit Presentation MVC

---
+++ MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,29 @@
+﻿using MicroRabbit.MVC.Models.Dtos;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "http://localhost:5000/api/banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto), Encoding.UTF8, "application/json");
+
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
